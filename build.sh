a0ede5113b136d5e7f9785fbdf119ed8304f44af
Andres Hojnadel <a.hojnadel@gmail.com>
2025-05-26 00:03:38 -0300

    Using GLEW for modern OpenGL

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
-INCLUDE_PATH="./deps/GLFW/include/"
-LIB_PATH="./deps/GLFW/lib/"
+GLFW_INCLUDE_PATH="./deps/GLFW/include/"
+GLFW_LIB_PATH="./deps/GLFW/lib/"
+GLFW_STATIC_LIB="./deps/GLFW/lib/libglfw3.a"
 
-LIBS="-lglfw3 -lpthread -lGL -lm -ldl -lX11 -lpthread"
+GLEW_INCLUDE_PATH="./deps/GLEW/include/"
+GLEW_LIB_PATH="./deps/GLEW/lib/"
+GLEW_STATIC_LIB="./deps/GLEW/lib/libGLEW.a"
+
+STATIC_LIBS="$GLEW_STATIC_LIB $GLFW_STATIC_LIB"
+LIBS="-lpthread -lGL -lm -ldl -lX11 -lpthread"
 
 OUT_FILE="app"
 
-# g++ -I$INCLUDE_PATH -L$LIB_PATH src/application.cpp -lpthread
-g++ -I$INCLUDE_PATH -L$LIB_PATH src/application.cpp $LIBS -o $OUT_FILE
+g++ -I$GLFW_INCLUDE_PATH -I$GLEW_INCLUDE_PATH -L$GLFW_LIB_PATH src/application.cpp $STATIC_LIBS $LIBS -o $OUT_FILE
--- src/application.cpp
+++ src/application.cpp
@@ -1,11 +1,15 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void) {
   GLFWwindow* window;
 
   /* Initialize the library */
-  if (!glfwInit())
+  if (!glfwInit()) {
     return -1;
+  }
 
   /* Create a windowed mode window and its OpenGL context */
   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -16,6 +20,14 @@ int main(void) {
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
+
+  /* Make context before call Init */
+  if (glewInit() != GLEW_OK) {
+    std::cout << "GLEW doesn't init properly" << std::endl;
+    return 1;
+  }
+
+  std::cout << "Using " << glGetString(GL_VERSION) << std::endl;
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
