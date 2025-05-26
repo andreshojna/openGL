21cc17fa6573c85e96ef6ba57623e24867392287
Andres Hojnadel <a.hojnadel@gmail.com>
2025-05-26 19:28:27 -0300

    Setting vertex attribute

--- src/application.cpp
+++ src/application.cpp
@@ -29,8 +29,9 @@ int main(void) {
 
   std::cout << "Using " << glGetString(GL_VERSION) << std::endl;
 
+  /* Each line is a vertex. In OpenGL, a vertex is the basic unit of geometry. It's a point in space that defines shapes like triangles, lines, and points. */
   float position_data[] = {
-    -0.5f, -0.5f,
+    -0.5f, -0.5f, // Each vertex is a vertice
     0.0f, 0.5f,
     0.5f, -0.5f,
   };
@@ -43,7 +44,23 @@ int main(void) {
   unsigned int buffer;      //  Buffer ID
   glGenBuffers(1, &buffer); //  Create buffer
   glBindBuffer(GL_ARRAY_BUFFER, buffer);  //  Select the buffer
-  glBufferData(GL_ARRAY_BUFFER, sizeof(position_data), position_data, GL_STATIC_DRAW);
+
+  glBufferData(GL_ARRAY_BUFFER,
+              sizeof(position_data),  // size in bytes
+              position_data,          // data address
+              GL_STATIC_DRAW);        // hint
+
+  /* Set the vertex atributes: that is, explain the layout. Possition, color, normal, texture, all are attributes */
+  // stride: amount of bytes between vertex: the size of each vertex depends on the defined attributes
+  // pointer: index in bytes inside the vertex, its like the offset where the attribute is inside the vertex
+  glVertexAttribPointer(0,    // Index:
+                        2,    // size: number of components per generic vertex attribute. (1 to 4)
+                        GL_FLOAT, // type
+                        GL_FALSE, // normalize
+                        sizeof(float) * 2, // stride: two floats per vertex = 8bytes
+                        0);  // pointer: we only have one attribute per vertex
+
+  glEnableVertexAttribArray(0);  // index to the attribute in the vertex
 
 
   /* Loop until the user closes the window */
