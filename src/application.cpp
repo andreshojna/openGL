f2bbb46cd089c82caa506f2041f9e3966701a6b4
Andres Hojnadel <a.hojnadel@gmail.com>
2025-05-27 17:59:09 -0300

    First vertex and fragment shaders

--- src/application.cpp
+++ src/application.cpp
@@ -3,6 +3,49 @@
 
 #include <iostream>
 
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+  unsigned int id = glCreateShader(type);
+  const char* src = source.c_str();
+  glShaderSource(id, 1, &src, nullptr);
+  glCompileShader(id);
+
+  // Error handling
+  int result;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+  if (result == GL_FALSE) {
+    int length;
+    glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+    char* message = (char*) alloca(length * sizeof(char)); // Alloc dynamically in stack
+    glGetShaderInfoLog(id, length, &length, message);
+    std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
+    std::cout << message << std::endl;
+    glDeleteShader(id);
+    return 0;
+  }
+
+  return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+  unsigned int program = glCreateProgram();
+
+  /* Compile shaders */
+  unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+  unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+  /* Attch shaders to the program */
+  glAttachShader(program, vs);
+  glAttachShader(program, fs);
+  glLinkProgram(program);
+  glValidateProgram(program);
+
+  /* Delete intermediates */
+  glDeleteShader(vs);
+  glDeleteShader(fs);
+
+  return program;
+}
+
 int main(void) {
   GLFWwindow* window;
 
@@ -30,7 +73,7 @@ int main(void) {
   std::cout << "Using " << glGetString(GL_VERSION) << std::endl;
 
   /* Each line is a vertex. In OpenGL, a vertex is the basic unit of geometry. It's a point in space that defines shapes like triangles, lines, and points. */
-  float position_data[] = {
+  float position[] = {
     -0.5f, -0.5f, // Each vertex is a vertice
     0.0f, 0.5f,
     0.5f, -0.5f,
@@ -46,8 +89,8 @@ int main(void) {
   glBindBuffer(GL_ARRAY_BUFFER, buffer);  //  Select the buffer
 
   glBufferData(GL_ARRAY_BUFFER,
-              sizeof(position_data),  // size in bytes
-              position_data,          // data address
+              sizeof(position),  // size in bytes
+              position,          // data address
               GL_STATIC_DRAW);        // hint
 
   /* Set the vertex atributes: that is, explain the layout. Possition, color, normal, texture, all are attributes */
@@ -62,6 +105,37 @@ int main(void) {
 
   glEnableVertexAttribArray(0);  // index to the attribute in the vertex
 
+  std::string vertexShader = 
+      // "#version 330 core \n"                   // not supported in HP Probook 4530s
+      "#version 130\n"                            // use this instead
+      "\n"
+      // "layout(location = 0) in vec4 position;" // not supported in version 1.30
+      "attribute vec4 position;\n"                // use this instead
+      "\n"
+      "void main() {\n"
+        "gl_Position = position;\n"
+      "}\n";
+  
+  std::string fragmentShader = 
+      // "#version 330 core \n"                   // not supported in HP Probook 4530s
+      "#version 130\n"                            // use this instead
+      // "layout(location = 0) out vec4 color;\n"
+      "\n"
+      "void main() {\n"
+        // "color = vec4(1.0, 0.0, 0.0, 1.0);\n"      // not supported in version 1.30
+        "gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"  // use this instead
+      "}\n";
+
+  unsigned int shader = CreateShader(vertexShader, fragmentShader);
+  glUseProgram(shader);
+
+  /** Pipeline
+   * ---------
+   * Draw call
+   * Vertex shader: is call once per vertex. We tell where that vertex to be.
+   * Fragment (pixel) shader: runs one per each pixel. Determines the color for each pixel.
+   * Visualisation
+   */
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
@@ -76,6 +150,8 @@ int main(void) {
     /* Poll for and process events */
     glfwPollEvents();
   }
+
+  glDeleteProgram(shader);
 
   glfwTerminate();
   return 0;
