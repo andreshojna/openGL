3e07db2ae9f0aa0b9b60f87bfdf42875745028d1
Andres Hojnadel <a.hojnadel@gmail.com>
2025-06-08 18:14:56 -0300

    Using ImGui to move model

--- src/application.cpp
+++ src/application.cpp
@@ -144,12 +144,10 @@ int main(void) {
   ImGui_ImplGlfwGL3_Init(window, true);
   ImGui::StyleColorsDark();
 
+  glm::vec3 translation = glm::vec3(200, 200, 0);
+
   float r = 0.0f;
   float inc = 0.025f;
-
-  bool show_demo_window = true;
-  bool show_another_window = false;
-  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window)) {
@@ -158,8 +156,12 @@ int main(void) {
 
     ImGui_ImplGlfwGL3_NewFrame();
 
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+    glm::mat4 mvp = proj * view * model;
+  
     shader.Bind();
     shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+    shader.SetUniformMat4f("u_MVP", mvp);
 
     renderer.Draw(va, ib, shader);
 
@@ -168,18 +170,7 @@ int main(void) {
     r += inc; 
 
     {
-      static float f = 0.0f;
-      static int counter = 0;
-      ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-      ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-      ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
-      ImGui::Checkbox("Another Window", &show_another_window);
-
-      if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-          counter++;
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", counter);
+      ImGui::SliderFloat3("translation", &translation.x, 0.0f, 860.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
 
       ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
     }
